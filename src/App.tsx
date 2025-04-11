c06d05b978f9de21e0671d45368cd9b3d483cc46
German <kostiakovg@gmail.com>
2025-04-11 09:38:15 +0300

    favorites count in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 import { Link, Outlet } from "react-router";
+import { RootState } from "./app/store";
 
 function App() {
+  const favorites = useSelector((state: RootState) => state.favorites.movies);
+
   return (
     <div>
       <nav>
         <Link to="/">Home</Link> | <Link to="/movies">Movies</Link> |{" "}
-        <Link to="/favorites">Favorites</Link>
+        <Link to="/favorites">Favorites{` (${favorites.length})`}</Link>
       </nav>
       <hr />
       <Outlet />
